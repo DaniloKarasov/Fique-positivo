27e064f12421b63160a8b31838f2a51aa12c3c65
Danilo  Queiroz <danilozerosix@gmail.com>
2023-11-29 21:56:24 -0300

    fix: trocar o tipo de forma de pagamento para enum

--- fiquepositivo/src/main/java/br/com/fiquepositivo/model/Gasto.java
+++ fiquepositivo/src/main/java/br/com/fiquepositivo/model/Gasto.java
@@ -1,13 +1,15 @@
 package br.com.fiquepositivo.model;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data
 public class Gasto {
-	private String data;
+	private LocalDate data;
 	private String descricao;
 	private Double valor;
 	private Boolean necessidade;
 	private String local;
-	private String formaPagamento;
+	private Enum formaPagamento;
 }
