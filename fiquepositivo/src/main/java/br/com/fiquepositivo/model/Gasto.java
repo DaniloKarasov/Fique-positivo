5c579c166513f8303cba545d35b3be193a417358
Danilo  Queiroz <danilozerosix@gmail.com>
2023-12-06 15:05:18 -0300

    feature: criando o banco de dados
    O banco de dados mysql foi criado usando o DDL generate.

--- fiquepositivo/src/main/java/br/com/fiquepositivo/model/Gasto.java
+++ fiquepositivo/src/main/java/br/com/fiquepositivo/model/Gasto.java
@@ -1,13 +1,39 @@
 package br.com.fiquepositivo.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Entity
 @Data
 public class Gasto {
-	private String data;
-	private String descricao;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Integer idGasto;
+	
+	@Column(nullable = false)
 	private Double valor;
-	private Boolean necessidade;
-	private String local;
+
+	@Column
 	private String formaPagamento;
+
+	@Column
+	private String data;
+	
+	@Column
+	private String local;
+
+	@Column
+	private String descricao;
+	
+	@Column
+	private Boolean necessidade;
+	
+	
 }
