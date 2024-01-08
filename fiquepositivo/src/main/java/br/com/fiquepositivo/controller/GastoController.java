03d1e6b95a489d98c89aa9a3e9f98f2fa916b4ef
Danilo  Queiroz <danilozerosix@gmail.com>
2024-01-08 19:48:06 -0300

    fix: removendo importação inutilizada

--- fiquepositivo/src/main/java/br/com/fiquepositivo/controller/GastoController.java
+++ fiquepositivo/src/main/java/br/com/fiquepositivo/controller/GastoController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.fiquepositivo.exceptions.ChaveEstrangeiraInvalidaException;
 import br.com.fiquepositivo.exceptions.IdNaoCadastradoException;
 import br.com.fiquepositivo.model.Gasto;
 import br.com.fiquepositivo.repository.GastoRepository;
