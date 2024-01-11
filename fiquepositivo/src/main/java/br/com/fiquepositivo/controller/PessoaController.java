4d764257f04fd4d951299f33266862864e1783e9
Danilo  Queiroz <danilozerosix@gmail.com>
2024-01-11 02:48:42 -0300

    feature: implementação do verbo http patch
    Quando atualizado um campo com Integer está dando erro.

--- fiquepositivo/src/main/java/br/com/fiquepositivo/controller/PessoaController.java
+++ fiquepositivo/src/main/java/br/com/fiquepositivo/controller/PessoaController.java
@@ -1,6 +1,7 @@
 package br.com.fiquepositivo.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -58,6 +60,16 @@ public class PessoaController {
 		try {
 			pessoa = pessoaService.atualizar(pessoaId, pessoa);
 			return ResponseEntity.ok(pessoa);	
+		} catch (IdNaoCadastradoException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
+	
+	@PatchMapping("/{pessoaId}")
+	public ResponseEntity<?> atualizarParcialmente (@PathVariable Integer pessoaId, @RequestBody Map<String, Object> dados) {
+		try {
+			Pessoa pessoa = pessoaService.atualizarParcialmente(pessoaId, dados);
+			return ResponseEntity.ok(pessoa);
 		} catch (IdNaoCadastradoException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
 		}
--- fiquepositivo/src/main/java/br/com/fiquepositivo/service/PessoaService.java
+++ fiquepositivo/src/main/java/br/com/fiquepositivo/service/PessoaService.java
@@ -1,11 +1,16 @@
 package br.com.fiquepositivo.service;
 
+import java.lang.reflect.Field;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.fiquepositivo.exceptions.ConflitoDeDadosException;
 import br.com.fiquepositivo.exceptions.IdNaoCadastradoException;
@@ -27,6 +32,25 @@ public class PessoaService {
 		if(pessoaExistente.isPresent()) {
 			BeanUtils.copyProperties(pessoa, pessoaExistente.get(), "id");
 			return salvar(pessoaExistente.get());
+		} else {
+			throw new IdNaoCadastradoException(String.format("Não existe pessoa com id %s.", pessoaId));
+		}
+	}
+	
+	public Pessoa atualizarParcialmente(Integer pessoaId, Map<String, Object> dados) {
+		Optional<Pessoa> pessoaExistente = pessoaRepository.findById(pessoaId);
+		ObjectMapper objectMapper = new ObjectMapper();
+		Pessoa pessoaDados = objectMapper.convertValue(dados, Pessoa.class);
+		if(pessoaExistente.isPresent()) {
+			dados.forEach((nomeAtributo, valorAtributo) -> {
+				Field field = ReflectionUtils.findField(Pessoa.class, nomeAtributo);
+				field.setAccessible(true);
+				
+				Object valorConvertido = ReflectionUtils.getField(field, pessoaDados);
+				ReflectionUtils.setField(field, pessoaExistente.get(), valorConvertido);
+		
+			});
+			return atualizar(pessoaId, pessoaExistente.get());
 		} else {
 			throw new IdNaoCadastradoException(String.format("Não existe pessoa com id %s.", pessoaId));
 		}
