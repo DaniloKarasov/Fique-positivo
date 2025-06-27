a2696f4afc083e08fb182531e0b184df44b28949
Danilo Queiroz <daniloqueiroz.dev@gmail.com>
2025-06-27 04:18:50 -0300

    feat: implementação dos tests de PessoaController
    Foi feito testes de sucesso e falha para as requisições de PessoaController.

--- fiquepositivo/src/test/java/br/com/fiquepositivo/api/controller/PessoaControllerTest.java
+++ fiquepositivo/src/test/java/br/com/fiquepositivo/api/controller/PessoaControllerTest.java
@@ -1,10 +1,9 @@
 package br.com.fiquepositivo.api.controller;
 
-import br.com.fiquepositivo.api.controller.PessoaController;
+import br.com.fiquepositivo.domain.exceptions.ConflitoDeDadosException;
 import br.com.fiquepositivo.domain.exceptions.IdNaoCadastradoException;
 import br.com.fiquepositivo.domain.model.Pessoa;
 import br.com.fiquepositivo.domain.service.PessoaService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +15,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PessoaController.class)
@@ -68,6 +68,7 @@ class PessoaControllerTest {
 
         mockMvc.perform(get("/pessoas/{idFalse}", idFalse)).andExpect(status().isNotFound())
                 .andExpect(content().string(String.format("Não existe pessoa com id %s.", idFalse)));
+        verify(pessoaService).buscar(idFalse);
     }
 
     @Test
@@ -83,17 +84,99 @@ class PessoaControllerTest {
                 .andExpect(jsonPath("$.nome").value(pessoaCreated.getNome()))
                 .andExpect(jsonPath("$.rendaMensal").value(pessoaCreated.getRendaMensal()))
                 .andExpect(jsonPath("$.profissao").value(pessoaCreated.getProfissao()));
+        verify(pessoaService).salvar(pessoaInput);
     }
 
     @Test
-    void atualizar() {
+    void testAtualizar() throws Exception {
+        Integer id = 1;
+        Pessoa pessoaInput = new Pessoa(null, "Marcelo", 3500.0, "Operador de máquinas");
+        Pessoa pessoaAtualizada = new Pessoa(id, "Marcelo", 3500.0, "Operador de máquinas");
+
+        when(pessoaService.atualizar(id, pessoaInput)).thenReturn(pessoaAtualizada);
+
+        mockMvc.perform(put("/pessoas/{id}", id).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(pessoaInput))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(pessoaAtualizada.getId()))
+                .andExpect(jsonPath("$.nome").value(pessoaAtualizada.getNome()))
+                .andExpect(jsonPath("$.rendaMensal").value(pessoaAtualizada.getRendaMensal()))
+                .andExpect(jsonPath("$.profissao").value(pessoaAtualizada.getProfissao()));
+        verify(pessoaService).atualizar(id, pessoaInput);
     }
 
     @Test
-    void atualizarParcialmente() {
+    void testAtualizarNaoEncontrado() throws Exception {
+        Integer idInvalido = 1;
+        Pessoa pessoaInput = new Pessoa(null, "Marcelo", 3500.0, "Operador de máquinas");
+        String mensagemErro = String.format("Não existe pessoa com id %s.", idInvalido);
+        when(pessoaService.atualizar(idInvalido, pessoaInput)).thenThrow(new IdNaoCadastradoException(mensagemErro));
+
+        mockMvc.perform(put("/pessoas/{id}", idInvalido).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(pessoaInput))).andExpect(status().isNotFound())
+                .andExpect(content().string(mensagemErro));
+        verify(pessoaService).atualizar(idInvalido, pessoaInput);
     }
 
     @Test
-    void excluir() {
+    void testAtualizarParcialmente() throws Exception {
+        Integer id = 1;
+        String jsonParcial = "{ \"profissao\": \"Analista\" }";
+
+        Pessoa pessoaAtualizada = new Pessoa(id, "Sara", 2500.0, "Analista");
+
+        when(pessoaService.atualizarParcialmente(eq(id), anyMap())).thenReturn(pessoaAtualizada);
+
+        mockMvc.perform(patch("/pessoas/{id}", id).contentType(MediaType.APPLICATION_JSON).content(jsonParcial))
+                .andExpect(status().isOk()).andExpect(jsonPath("$.id").value(pessoaAtualizada.getId()))
+                .andExpect(jsonPath("$.nome").value(pessoaAtualizada.getNome()))
+                .andExpect(jsonPath("$.rendaMensal").value(pessoaAtualizada.getRendaMensal()))
+                .andExpect(jsonPath("$.profissao").value(pessoaAtualizada.getProfissao()));
+
+        verify(pessoaService).atualizarParcialmente(eq(id), anyMap());
+    }
+
+    @Test
+    void testAtualizarParcialmenteNaoEncontrado() throws Exception {
+        Integer idInvalido = 1;
+        String jsonParcial = "{ \"profissao\": \"Analista\" }";
+        String mensagemErro = String.format("Não existe pessoa com id %s.", idInvalido);
+
+        when(pessoaService.atualizarParcialmente(eq(idInvalido), anyMap())).thenThrow(
+                new IdNaoCadastradoException(mensagemErro));
+
+        mockMvc.perform(patch("/pessoas/{id}", idInvalido).contentType(MediaType.APPLICATION_JSON).content(jsonParcial))
+                .andExpect(status().isNotFound()).andExpect(content().string(mensagemErro));
+
+        verify(pessoaService).atualizarParcialmente(eq(idInvalido), anyMap());
+    }
+
+    @Test
+    void testExcluir() throws Exception {
+        Integer id = 1;
+        doNothing().when(pessoaService).excluir(id);
+        mockMvc.perform(delete("/pessoas/{id}", id)).andExpect(status().isNoContent());
+        verify(pessoaService).excluir(id);
+    }
+
+    @Test
+    void testExcluirNaoEncontrado() throws Exception {
+        Integer idInvalido = 1;
+        String mensagemErro = String.format("Não existe pessoa com id %s.", idInvalido);
+        doThrow(new IdNaoCadastradoException(mensagemErro)).when(pessoaService).excluir(idInvalido);
+        mockMvc.perform(delete("/pessoas/{id}", idInvalido)).andExpect(status().isNotFound())
+                .andExpect(content().string(mensagemErro));
+        verify(pessoaService).excluir(idInvalido);
+    }
+
+    @Test
+    void testExcluirConflito() throws Exception {
+        Integer idConflito = 2;
+        String mensagemErro =
+                String.format("A pessoa com o id %s não pode ser excluída porque existe gasto/s vinculado/s a ela.",
+                        idConflito);
+        doThrow(new ConflitoDeDadosException(mensagemErro)).when(pessoaService).excluir(idConflito);
+        mockMvc.perform(delete("/pessoas/{id}", idConflito)).andExpect(status().isConflict())
+                .andExpect(content().string(mensagemErro));
+        verify(pessoaService).excluir(idConflito);
     }
 }
