e89d8ee02d204169adc5ee4608b966e276a61a3d
Danilo Queiroz <daniloqueiroz.dev@gmail.com>
2025-07-03 00:35:33 -0300

    feat: criado tests unitários para pessoaService

--- fiquepositivo/src/test/java/br/com/fiquepositivo/api/controller/PessoaControllerTest.java
+++ fiquepositivo/src/test/java/br/com/fiquepositivo/api/controller/PessoaControllerTest.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -204,5 +205,39 @@ class PessoaControllerTest {
                 .andExpect(jsonPath("$.status").value(409))
                 .andExpect(jsonPath("$.message").value(mensagemErro));
         verify(pessoaService).excluir(idConflito);
+    }
+
+    @Test
+    void testErroSintaxeJson() throws Exception {
+        String jsonMalFormado = "{\n" +
+                "    nome\": \"Hercules\",\n" +
+                "    \"profissao\": \"Fazendeiro\",\n" +
+                "    \"rendaMensal\": 10000.0\n" +
+                "}";
+        String mensagemErro = "Erro de sintaxe no JSON enviado.";
+
+        mockMvc.perform(post("/pessoas").contentType(MediaType.APPLICATION_JSON).content(jsonMalFormado))
+                .andExpect(status().isBadRequest()).andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.message").value(mensagemErro));
+    }
+
+    @Test
+    void deveRetornarErrosDeValidacaoQuandoCamposInvalidos() throws Exception {
+        // JSON com campos inválidos (nome em branco e rendaMensal negativa)
+        String jsonPessoaInvalida = """
+                {
+                    "nome": "",
+                    "rendaMensal": -100,
+                    "profissao": "Programador"
+                }
+                """;
+
+        mockMvc.perform(post("/pessoas")              // ajuste a URL conforme seu endpoint
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonPessoaInvalida))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(content().string(containsString("O nome é obrigatório.")))
+                .andExpect(content().string(containsString("O campo renda mensal não pode ser negativo.")));
     }
 }
--- fiquepositivo/src/test/java/br/com/fiquepositivo/domain/service/PessoaServiceTest.java
+++ fiquepositivo/src/test/java/br/com/fiquepositivo/domain/service/PessoaServiceTest.java
@@ -1,18 +1,22 @@
 package br.com.fiquepositivo.domain.service;
 
+import br.com.fiquepositivo.domain.exceptions.IdNaoCadastradoException;
 import br.com.fiquepositivo.domain.model.Pessoa;
 import br.com.fiquepositivo.domain.repository.PessoaRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PessoaServiceTest {
@@ -33,28 +37,87 @@ class PessoaServiceTest {
 
         List<Pessoa> result = pessoaService.listar();
 
-        assertEquals(pessoaList.size(), result.size());
-        assertEquals(pessoaList.get(0).getId(), result.get(0).getId());
-        assertEquals(pessoaList.get(1).getNome(), result.get(1).getNome());
+        assertEquals(pessoaList, result);
     }
 
     @Test
-    void buscar() {
+    void testBuscar() {
+        Integer id = 1;
+        Pessoa pessoa = new Pessoa(1, "Fábio", 5000.0, "Analista de sistemas");
+
+        when(pessoaRepository.findById(id)).thenReturn(Optional.of(pessoa));
+
+        Pessoa result = pessoaService.buscar(id);
+
+        assertEquals(pessoa, result);
     }
 
     @Test
-    void salvar() {
+    void testSalvar() {
+        Pessoa pessoa = new Pessoa(null, "Fábio", 5000.0, "Analista de sistemas");
+        Pessoa pessoaSalva = new Pessoa(1, "Fábio", 5000.0, "Analista de sistemas");
+
+        when(pessoaRepository.save(pessoa)).thenReturn(pessoaSalva);
+
+        Pessoa result = pessoaService.salvar(pessoa);
+
+        assertEquals(pessoaSalva, result);
     }
 
     @Test
     void atualizar() {
+        Integer id = 1;
+        Pessoa pessoaExistente = new Pessoa(id, "Fábio", 5000.0, "Analista de sistemas");
+        Pessoa novosDados = new Pessoa(null, "Fábio Augusto", 5500.0, "Engenheiro de software");
+        Pessoa pessoaAtualizada = new Pessoa(id, "Fábio Augusto", 5500.0, "Engenheiro de software");
+
+        when(pessoaRepository.findById(id)).thenReturn(Optional.of(pessoaExistente));
+        when(pessoaRepository.save(pessoaExistente)).thenReturn(pessoaExistente);
+
+        Pessoa result = pessoaService.atualizar(id, novosDados);
+
+        assertEquals(pessoaAtualizada, result);
     }
 
     @Test
     void atualizarParcialmente() {
+        Integer id = 1;
+        Pessoa pessoaExistente = new Pessoa(id, "Fábio", 5000.0, "Analista de sistemas");
+        Map<String, Object> dados = new HashMap<>();
+        dados.put("profissao", null);
+        dados.put("rendaMensal", 200.0);
+        Pessoa pessoaAtualizada = new Pessoa(id, "Fábio", 200.0, null);
+
+        when(pessoaRepository.findById(id)).thenReturn(Optional.of(pessoaExistente));
+        when(pessoaRepository.save(pessoaExistente)).thenReturn(pessoaExistente);
+
+        Pessoa result = pessoaService.atualizarParcialmente(id, dados);
+
+        assertEquals(pessoaAtualizada, result);
     }
 
     @Test
     void excluir() {
+        Integer id = 1;
+        Pessoa pessoa = new Pessoa(id, "Fábio", 5000.0, "Analista de sistemas");
+
+        when(pessoaRepository.findById(id)).thenReturn(Optional.of(pessoa));
+        doNothing().when(pessoaRepository).delete(pessoa);
+
+        pessoaService.excluir(id);
+
+        verify(pessoaRepository).findById(id);
+        verify(pessoaRepository).delete(pessoa);
+
+    }
+
+    @Test
+    void testExcluirIdInvalido() {
+        Integer id = 4;
+
+        when(pessoaRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(IdNaoCadastradoException.class, () -> pessoaService.excluir(id));
+        verify(pessoaRepository, never()).delete(any());
     }
 }
